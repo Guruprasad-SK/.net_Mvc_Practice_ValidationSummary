b6e0d05397b708af930cd2f7e616b06469fe9ba1
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-11-03 17:59:10 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationInMvc.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Please Enter your first name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please Enter your last name")]
+        public string LasttName { get; set; }
+        [Required(ErrorMessage = "Please Enter your valid Email")]
+        [EmailAddress]
+        public string  Email{ get; set; }
+        [Required(ErrorMessage = "Please Enter your Age between 1-100")]
+        [Range(0,100)]
+        public int Age { get; set; }
+    }
+}
